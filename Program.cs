e498b7d811a44a0b00b8f5e3aa7fae65386c06a4
victormichelsilva <victormichelsilva@gmail.com>
2021-12-06 20:00:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Prototype1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria objeto do tipo Soldado
+            Soldado soldado = new Soldado();
+            soldado.Nome = "Soldado";
+            soldado.Arma = "Fuzil";
+
+            Soldado soldado_clone1 = (Soldado)soldado.Clone();
+            Soldado soldado_clone2 = (Soldado)soldado.Clone();
+
+            //Exibe valores do objeto original
+            WriteLine(">> Objeto Original");
+            WriteLine($"{soldado.Nome} - {soldado.Arma}");
+
+            //Valores de objeto clone1
+            WriteLine(">> Objeto Clone1");
+            WriteLine($"{soldado_clone1.Nome} - {soldado_clone1.Arma}");
+
+            //Valores de objeto clone2
+            WriteLine(">> Objeto Clone2");
+            WriteLine($"{soldado_clone2.Nome} - {soldado_clone2.Arma}");
+        }
+    }
+}
---
+++ Soldado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Prototype1
+{
+    public class Soldado : ICloneable
+    {
+        public string Nome { get; set; }
+
+        public string Arma { get; set; }
+
+        public Soldado() { }
+
+        public Soldado(Soldado s) 
+        {
+            this.Nome = s.Nome;
+            this.Arma = s.Arma;
+        }
+
+        public object Clone()
+        {
+            return new Soldado(this);
+        }
+    }
+}
